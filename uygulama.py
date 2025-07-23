0346a770aa23a1e045ab10e578e97416c10285a5
ays786 <ayseakay1989@gmail.com>
2025-07-23 16:16:24 +0300

    Notları gösterme

--- uygulama.py
+++
@@ -1,8 +0,0 @@
-def not_ekle():
-    not_metni = input("Notunuzu yazın: ")
-    with open("notlar.txt", "a") as dosya:
-        dosya.write(not_metni + "\n")
-    print("Notunuz kaydedildi!")
-
-if __name__ == "__main__":
-    not_ekle()
